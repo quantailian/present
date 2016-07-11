9fbebf76a7985eee8b0ac814babc29efeaafdefb
quantailian <www.lianlian.com.c@qq.com>
2016-07-11 19:14:26 +0800

    我的礼物说项目

---
+++ MyGiftDemo/app/src/main/java/comnd/example/dllo/mygiftdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package comnd.example.dllo.mygiftdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
